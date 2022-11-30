a5c4360c175386441c128993d6c6931b4cb2d4b7
klmana <104742971+klmana@users.noreply.github.com>
2022-12-01 08:10:36 +1000

    fix pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,13 +16,14 @@ def states():
     """
 
     return render_template(
-        '7-states_list.html', states=[st for st in storage.all('State').values()])
+        states=[st for st in storage.all('State').values()])
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the database again at the end of the request."""
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
